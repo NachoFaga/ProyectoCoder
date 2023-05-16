acf03306fe776cbb138c363c14b5930b855cf2ae
NachoFaga <ignacio.faga@gmail.com>
2023-05-16 13:19:08 -0300

    primerpreentrega

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,48 @@ toggle.addEventListener('click', () => {
     toggle.classList.toggle('rotate')
     links.classList.toggle('active')
 })
+
+
+/* Lo de arriba no lo hice yo */
+
+
+let testJS = 5 
+
+if (testJS == 5){
+    console.log("El JS funciona correctamente!")
+}
+
+let nombreUsuario = prompt("Hola bienvendio! cual es tu nombre?")
+
+if (nombreUsuario == "") {
+    alert("Hola?")
+}
+else {
+    alert("Bienvenido, " + nombreUsuario + "!")
+}
+
+let servicio = prompt("Un gusto " + nombreUsuario +", Estas aqui de 'Visita' o buscas 'Presupuesto'?");
+
+if((servicio == "Visita") || (servicio == "Presupuesto")){
+    alert("Seleccionaste " + servicio + ".")
+}else{
+    alert("Ingrese una opcion valida porfavor.")
+}
+
+
+
+function presupuesto (primerNumero, segundoNumero, operacion){
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero
+        case "-":
+            return primerNumero - segundoNumero
+        case "*":
+            return primerNumero * segundoNumero
+        case "/":
+            return primerNumero / segundoNumero
+        default:
+            return primerNumero * segundoNumero
+    }
+}
+console.log(presupuesto(10, 2, "*"));
